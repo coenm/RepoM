92719e78afa7b4ea3f828e5273a5c289321d034d
Sjoerd Heijligers <Sjoerdsjoerd@users.noreply.github.com>
2023-02-10 14:21:36 +0100

    Implemented ReposRootDirectories (#52)

--- src/RepoM.Api/IO/DefaultDriveEnumerator.cs
+++ src/RepoM.Api/IO/DefaultDriveEnumerator.cs
@@ -1,23 +1,50 @@
 namespace RepoM.Api.IO;
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using System.Linq;
+using RepoM.Api.Common;
 
 public class DefaultDriveEnumerator : IPathProvider
 {
+    private readonly IAppSettingsService _appsettings;
     private readonly IFileSystem _fileSystem;
 
-    public DefaultDriveEnumerator(IFileSystem fileSystem)
+    public DefaultDriveEnumerator(IAppSettingsService appsettings, IFileSystem fileSystem)
     {
+        _appsettings = appsettings;
         _fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
     }
 
     public string[] GetPaths()
     {
+        // Check if the ReposRootDirectories is valid and use that for searching repos
+        if (_appsettings.ReposRootDirectories.Any())
+        {
+            HashSet<string> paths = new();
+
+            foreach (var reposRootDirectoryPath in _appsettings.ReposRootDirectories)
+            {
+                DirectoryInfo reposRootDirectory = new(reposRootDirectoryPath);
+
+                if (reposRootDirectory.Exists)
+                {
+                    _ = paths.Add(reposRootDirectoryPath);
+                }
+            }
+
+            if (paths.Any())
+            {
+                return paths.ToArray();
+            }
+        }
+
+        // By default search all drives for repos
         return _fileSystem.DriveInfo.GetDrives()
-                          .Where(d => d.DriveType == System.IO.DriveType.Fixed)
-                          .Select(d => d.RootDirectory.FullName)
-                          .ToArray();
+                         .Where(d => d.DriveType == DriveType.Fixed)
+                         .Select(d => d.RootDirectory.FullName)
+                         .ToArray();
     }
 }
