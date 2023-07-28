b3083a7001a562218811b8f5b891f228def5f1ef
Coen van den Munckhof <c.munckhof@gmail.com>
2023-07-28 12:17:54 +0200

    added tests

--- tests/RepoM.Plugin.Misc.Tests/PackageTests.cs
+++ tests/RepoM.Plugin.Misc.Tests/PackageTests.cs
@@ -4,34 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using RepoM.Core.Plugin;
-using RepoM.Plugin.AzureDevOps;
-using RepoM.Plugin.Clipboard;
-using RepoM.Plugin.EverythingFileSearch;
-using RepoM.Plugin.Heidi;
-using RepoM.Plugin.LuceneQueryParser;
-using RepoM.Plugin.SonarCloud;
-using RepoM.Plugin.Statistics;
-using RepoM.Plugin.WindowsExplorerGitInfo;
 using Xunit;
 
 public class PackageTests
 {
-    public static IEnumerable<object[]> PackagesTestData => Packages.Select(package => new object[] { package, });
-
-    private static IEnumerable<IPackage> Packages
-    {
-        get
-        {
-            yield return new AzureDevOpsPackage();
-            yield return new ClipboardPackage();
-            yield return new EverythingPackage();
-            yield return new HeidiPackage();
-            yield return new LuceneQueryParserPackage();
-            yield return new SonarCloudPackage();
-            yield return new StatisticsPackage();
-            yield return new WindowsExplorerGitInfoPackage();
-        }
-    }
+    public static IEnumerable<object[]> PackagesTestData => PluginStore.Packages.Select(package => new object[] { package, });
 
     [Theory]
     [MemberData(nameof(PackagesTestData))]
@@ -43,6 +20,6 @@ public class PackageTests
     [Fact]
     public void Name_ShouldBeUnique()
     {
-        Packages.Should().OnlyHaveUniqueItems(package => package.Name);
+        PluginStore.Packages.Should().OnlyHaveUniqueItems(package => package.Name);
     }
 }
