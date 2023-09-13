7333d5682a9aed0040943820481a2551c7083409
Coen van den Munckhof <c.munckhof@gmail.com>
2023-09-13 11:09:52 +0200

    update

--- src/RepoM.Plugin.WebBrowser/Services/IWebBrowserService.cs
+++ src/RepoM.Plugin.WebBrowser/Services/IWebBrowserService.cs
@@ -2,8 +2,6 @@ namespace RepoM.Plugin.WebBrowser.Services;
 
 internal interface IWebBrowserService
 {
-    bool ProfileExist(string name);
-
     void OpenUrl(string url);
 
     void OpenUrl(string url, string profile);
