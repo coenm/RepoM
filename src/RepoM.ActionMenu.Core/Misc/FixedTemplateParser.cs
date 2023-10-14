86b270c4cd60964ea7da4cef4e70d4a823fd23d1
Coen van den Munckhof <c.munckhof@gmail.com>
2023-10-14 23:07:36 +0200

    Udaate

--- src/RepoM.ActionMenu.Core/Misc/FixedTemplateParser.cs
+++ src/RepoM.ActionMenu.Core/Misc/FixedTemplateParser.cs
@@ -1,5 +1,6 @@
 namespace RepoM.ActionMenu.Core.Misc;
 
+using System;
 using RepoM.ActionMenu.Core.Model;
 using Scriban;
 using Scriban.Parsing;
@@ -16,14 +17,22 @@ internal class FixedTemplateParser : ITemplateParser
     public Template ParseScriptOnly(string text)
     {
         var template = Template.Parse(text, sourceFilePath: null!, _parserOptions, Lexers.ScriptOnly);
-        template.ThrowOnError();
+        ThrowOnError(template); 
         return template;
     }
 
     public Template ParseMixed(string text)
     {
         var template = Template.Parse(text, sourceFilePath: null!, _parserOptions, Lexers.Mixed);
-        template.ThrowOnError();
+        ThrowOnError(template);
         return template;
+    }
+
+    private static void ThrowOnError(Template template)
+    {
+        if (template.HasErrors)
+        {
+            throw new Exception($"Template has errors {template.Messages}");
+        }
     }
 }
