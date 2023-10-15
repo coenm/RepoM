1fd89a6fa740f6e1f205c954b4194ba84f010003
Coen van den Munckhof <c.munckhof@gmail.com>
2023-10-15 21:45:39 +0200

    update

--- src/RepoM.ActionMenu.Interface/Attributes/ActionMenuMemberAttribute.cs
+++ src/RepoM.ActionMenu.Interface/Attributes/ActionMenuMemberAttribute.cs
@@ -5,19 +5,12 @@ using System;
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
 public class ActionMenuMemberAttribute : Attribute
 {
-    public ActionMenuMemberAttribute(string alias) : this(alias, string.Empty)
-    {
-    }
-
-    public ActionMenuMemberAttribute(string alias, string category)
+    public ActionMenuMemberAttribute(string alias)
     {
         Alias = alias;
-        Category = category;
     }
 
     public string Alias { get; }
-
-    public string Category { get; }
-
+    
     public bool Functor { get; set; }
 }
