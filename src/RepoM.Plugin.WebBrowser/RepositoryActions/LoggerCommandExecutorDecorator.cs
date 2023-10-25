322876a4385d64a920d2eb3bc8ac4f5c081a6ce0
Coen van den Munckhof <c.munckhof@gmail.com>
2023-10-25 23:26:57 +0200

    ..

--- src/RepoM.Plugin.WebBrowser/RepositoryActions/LoggerCommandExecutorDecorator.cs
+++ src/RepoM.Plugin.WebBrowser/RepositoryActions/LoggerCommandExecutorDecorator.cs
@@ -1,10 +1,12 @@
 namespace RepoM.Plugin.WebBrowser.RepositoryActions;
 
 using System;
+using JetBrains.Annotations;
 using RepoM.Core.Plugin.Repository;
 using RepoM.Core.Plugin.RepositoryActions;
 using RepoM.Plugin.WebBrowser.RepositoryActions.Actions;
 
+[UsedImplicitly]
 internal class CoreBrowseRepositoryCommandExecutorDecorator : ICommandExecutor<Core.Plugin.RepositoryActions.Commands.BrowseRepositoryCommand>
 {
     private readonly ICommandExecutor<BrowseRepositoryCommand> _pluginBrowseCommandExecutor;
