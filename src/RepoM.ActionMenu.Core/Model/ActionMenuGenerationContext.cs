25400a377d8285053e95c74a9c4366f0f0a723ce
Coen van den Munckhof <c.munckhof@gmail.com>
2023-11-04 11:55:27 +0100

    Update ActionMenuGenerationContext.cs

--- src/RepoM.ActionMenu.Core/Model/ActionMenuGenerationContext.cs
+++ src/RepoM.ActionMenu.Core/Model/ActionMenuGenerationContext.cs
@@ -57,7 +57,6 @@ internal class ActionMenuGenerationContext : TemplateContext, IActionMenuGenerat
         var rootScriptObject = new RepoMScriptObject();
         
         rootScriptObject.SetValue("file", new FileFunctions(), true);
-        rootScriptObject.SetValue("web", new WebFunctions(), true);
         rootScriptObject.SetValue("repository", new RepositoryFunctions(Repository), true);
 
         Env = new EnvSetScriptObject(EnvScriptObject.Create());
