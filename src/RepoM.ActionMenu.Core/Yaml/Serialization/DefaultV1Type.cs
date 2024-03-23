ecc57808035d052f5dcf716f4df2c126cb9d43a9
Coen van den Munckhof <c.munckhof@gmail.com>
2024-03-23 11:45:46 +0100

    update

--- src/RepoM.ActionMenu.Core/Yaml/Serialization/DefaultV1Type.cs
+++ src/RepoM.ActionMenu.Core/Yaml/Serialization/DefaultV1Type.cs
@@ -1,7 +1,9 @@
 namespace RepoM.ActionMenu.Core.Yaml.Serialization;
 
+using System.Diagnostics.CodeAnalysis;
 using RepoM.ActionMenu.Core.Yaml.Model.ActionContext.SetVariable;
 
+[SuppressMessage("Code Smell", "S2094:Classes should not be empty", Justification = "Intentionally empty. Used as fallback for deserialization.")]
 internal class DefaultV1Type : ContextActionSetVariableV1
 {
     // intentionally empty.
