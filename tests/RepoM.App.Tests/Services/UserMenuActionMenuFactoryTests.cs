de494c1e4b3e142183c32c5fe3830649a305b327
Coen van den Munckhof <c.munckhof@gmail.com>
2024-10-11 20:53:51 +0200

    add tests

--- tests/RepoM.App.Tests/Services/UserMenuActionMenuFactoryTests.cs
+++ tests/RepoM.App.Tests/Services/UserMenuActionMenuFactoryTests.cs
@@ -1,16 +1,33 @@
 namespace RepoM.App.Tests.Services;
 
 using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
+using System.Linq;
+using System.Threading.Tasks;
 using FakeItEasy;
 using FluentAssertions;
 using RepoM.ActionMenu.Core;
+using RepoM.ActionMenu.Interface.UserInterface;
 using RepoM.App.Services;
 using RepoM.Core.Plugin.Common;
+using RepoM.Core.Plugin.Repository;
+using SimpleInjector.Diagnostics;
 using Xunit;
 
 public class UserMenuActionMenuFactoryTests
 {
+    private IUserInterfaceActionMenuFactory _factory;
+    private IAppDataPathProvider _appDataPathProvider;
+    private IFileSystem _fileSystem;
+
+    public UserMenuActionMenuFactoryTests()
+    {
+        _factory = A.Fake<IUserInterfaceActionMenuFactory>();
+        _appDataPathProvider = A.Fake<IAppDataPathProvider>();
+        _fileSystem = A.Fake<IFileSystem>();
+    }
+
     [Fact]
     public void Ctor_ShouldThrow_WhenArgumentNull()
     {
@@ -25,5 +42,22 @@ public class UserMenuActionMenuFactoryTests
         act1.Should().Throw<ArgumentNullException>();
         act2.Should().Throw<ArgumentNullException>();
         act3.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public async Task CreateMenuAsync_ShouldReturnErrorMenu_WhenFileNotExists()
+    {
+        // arrange
+        A.CallTo(() => _fileSystem.File.Exists(A<string>._)).Returns(false);
+        A.CallTo(() => _appDataPathProvider.AppDataPath).Returns("C:\\AppData");
+
+        var sut = new UserMenuActionMenuFactory(_fileSystem, _appDataPathProvider, _factory);
+
+        // act
+        List<UserInterfaceRepositoryActionBase> result = await sut.CreateMenuAsync(A.Dummy<IRepository>()).ToListAsync();
+
+        // assert
+        result.Should().HaveCount(3);
+        A.CallTo(_factory).MustNotHaveHappened();
     }
 }
