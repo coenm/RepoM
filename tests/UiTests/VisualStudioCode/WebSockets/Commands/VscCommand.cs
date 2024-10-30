ea9fdd712e6d14b1accfd300d4eb57b2546d50b9
Coen van den Munckhof <c.munckhof@gmail.com>
2024-10-30 22:15:38 +0100

    ..

--- tests/UiTests/VisualStudioCode/WebSockets/Commands/VscCommand.cs
+++ tests/UiTests/VisualStudioCode/WebSockets/Commands/VscCommand.cs
@@ -1,7 +1,5 @@
 namespace UiTests.VisualStudioCode.WebSockets.Commands;
 
-using System.Text.Json.Serialization;
-
 public class VscCommand
 {
     public VscCommand(string command)
@@ -9,12 +7,9 @@ public class VscCommand
         Command = command;
     }
 
-    [JsonPropertyOrder(-100)]
     public int Id { get; set; }
 
-    [JsonPropertyOrder(-99)]
     public string Type => "command";
 
-    [JsonPropertyOrder(-98)]
     public string Command { get; }
 }
