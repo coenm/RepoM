80182589b12ec29ce848646d12c8af475a182806
Coen van den Munckhof <c.munckhof@gmail.com>
2026-03-02 13:11:27 +0100

    refactor: update Watch methods to use async/await for better performance and readability

--- tests/RepoM.Core.Repositories.Tests/Watching/FileSystemRepositoryWatcherTests.cs
+++ tests/RepoM.Core.Repositories.Tests/Watching/FileSystemRepositoryWatcherTests.cs
@@ -7,6 +7,7 @@ using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using System.Reactive.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using AwesomeAssertions;
 using FakeItEasy;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -57,7 +58,7 @@ public class FileSystemRepositoryWatcherTests : IDisposable
     }
 
     [Fact]
-    public void Watch_ShouldNotEmitEvents_WhenPathDoesNotExist()
+    public async Task Watch_ShouldNotEmitEvents_WhenPathDoesNotExist()
     {
         // Act - Watch is long-running (never completes), so use Take with timeout
         var hasEvents = false;
@@ -65,7 +66,7 @@ public class FileSystemRepositoryWatcherTests : IDisposable
             .Take(TimeSpan.FromMilliseconds(200))
             .Subscribe(_ => hasEvents = true);
 
-        Thread.Sleep(300);
+        await Task.Delay(300);
 
         // Assert
         hasEvents.Should().BeFalse();
@@ -83,162 +84,205 @@ public class FileSystemRepositoryWatcherTests : IDisposable
     }
 
     [Fact]
-    public void Watch_ShouldEmitAddedEvent_WhenHeadFileIsCreated()
+    public async Task Watch_ShouldEmitAddedEvent_WhenHeadFileIsCreated()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act
         dummyWatcher.SimulateCreated(@"c:\repos\myrepo\.git\logs", "HEAD");
 
-        // Assert - debounce is 500ms, wait for it
-        Thread.Sleep(700);
+        // Assert
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
         events.Should().ContainSingle();
         events[0].ChangeType.Should().Be(RepositoryChangeType.Added);
         events[0].Path.Should().Be(@"c:\repos\myrepo\.git\logs\HEAD");
     }
 
     [Fact]
-    public void Watch_ShouldEmitModifiedEvent_WhenHeadFileIsChanged()
+    public async Task Watch_ShouldEmitModifiedEvent_WhenHeadFileIsChanged()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git\logs", "HEAD");
 
         // Assert
-        Thread.Sleep(700);
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
         events.Should().ContainSingle();
         events[0].ChangeType.Should().Be(RepositoryChangeType.Modified);
     }
 
     [Fact]
-    public void Watch_ShouldEmitRemovedEvent_WhenHeadFileIsDeleted()
+    public async Task Watch_ShouldEmitRemovedEvent_WhenHeadFileIsDeleted()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act
         dummyWatcher.SimulateDeleted(@"c:\repos\myrepo\.git\logs", "HEAD");
 
         // Assert
-        Thread.Sleep(700);
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
         events.Should().ContainSingle();
         events[0].ChangeType.Should().Be(RepositoryChangeType.Removed);
     }
 
     [Fact]
-    public void Watch_ShouldEmitModifiedEvent_WhenHeadFileIsRenamed()
+    public async Task Watch_ShouldEmitModifiedEvent_WhenHeadFileIsRenamed()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act
         dummyWatcher.SimulateRenamed(@"c:\repos\myrepo\.git\logs", "HEAD", "HEAD.old");
 
         // Assert
-        Thread.Sleep(700);
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
         events.Should().ContainSingle();
         events[0].ChangeType.Should().Be(RepositoryChangeType.Modified);
     }
 
     [Fact]
-    public void Watch_ShouldIgnoreNonGitPaths()
+    public async Task Watch_ShouldIgnoreNonGitPaths()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act - file not in a .git directory
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\src", "HEAD");
 
-        // Assert
-        Thread.Sleep(700);
+        // Assert - no event expected, wait briefly to confirm
+        mre.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
         events.Should().BeEmpty();
     }
 
     [Fact]
-    public void Watch_ShouldIgnoreNonHeadFiles()
+    public async Task Watch_ShouldIgnoreNonHeadFiles()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act - file in .git but not HEAD
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git", "config");
 
-        // Assert
-        Thread.Sleep(700);
+        // Assert - no event expected, wait briefly to confirm
+        mre.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
         events.Should().BeEmpty();
     }
 
     [Fact]
-    public void Watch_ShouldDebounceRapidChangesForSameRepository()
+    public async Task Watch_ShouldDebounceRapidChangesForSameRepository()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act - rapid changes to the same repo
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git\logs", "HEAD");
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git\logs", "HEAD");
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git\logs", "HEAD");
 
-        // Assert - only one event after debounce
-        Thread.Sleep(700);
+        // Assert - wait for first (and only) debounced event
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
+
+        // Wait a bit more to confirm no additional events arrive
+        await Task.Delay(700);
         events.Should().ContainSingle();
     }
 
     [Fact]
-    public void Watch_ShouldEmitSeparateEventsForDifferentRepositories()
+    public async Task Watch_ShouldEmitSeparateEventsForDifferentRepositories()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new CountdownEvent(2);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Signal();
+        });
 
         // Act - changes to different repos
         dummyWatcher.SimulateChanged(@"c:\repos\repo1\.git\logs", "HEAD");
         dummyWatcher.SimulateChanged(@"c:\repos\repo2\.git\logs", "HEAD");
 
         // Assert - two events, one per repo
-        Thread.Sleep(700);
+        mre.Wait(TimeSpan.FromSeconds(5)).Should().BeTrue();
         events.Should().HaveCount(2);
     }
 
@@ -263,7 +307,7 @@ public class FileSystemRepositoryWatcherTests : IDisposable
     }
 
     [Fact]
-    public void Watch_ShouldHandleWatcherCreationFailure()
+    public async Task Watch_ShouldHandleWatcherCreationFailure()
     {
         // Arrange
         var fileSystem = A.Fake<IFileSystem>();
@@ -272,33 +316,41 @@ public class FileSystemRepositoryWatcherTests : IDisposable
         var sut = new FileSystemRepositoryWatcher(fileSystem, NullLogger.Instance);
 
         // Act - should not throw, just log the error
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        using var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
-        Thread.Sleep(200);
-
-        // Assert
+        // Assert - no event expected
+        mre.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
         events.Should().BeEmpty();
     }
 
     [Fact]
-    public void Watch_ShouldStopRaisingEvents_WhenSubscriptionDisposed()
+    public async Task Watch_ShouldStopRaisingEvents_WhenSubscriptionDisposed()
     {
         // Arrange
         var dummyWatcher = new DummyFileSystemWatcher();
         var (fileSystem, sut) = CreateSutWithFakedFileSystem(dummyWatcher);
         A.CallTo(() => fileSystem.Directory.Exists(@"c:\repos")).Returns(true);
 
+        using var mre = new ManualResetEventSlim(false);
         var events = new List<RepositoryChangeEvent>();
-        var subscription = sut.Watch([@"c:\repos",]).Subscribe(e => events.Add(e));
+        var subscription = sut.Watch([@"c:\repos",]).Subscribe(e =>
+        {
+            events.Add(e);
+            mre.Set();
+        });
 
         // Act
         subscription.Dispose();
         dummyWatcher.SimulateChanged(@"c:\repos\myrepo\.git\logs", "HEAD");
 
-        Thread.Sleep(700);
-
-        // Assert
+        // Assert - no event expected
+        mre.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
         events.Should().BeEmpty();
     }
 
