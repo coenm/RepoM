a89865bc2e2f6cd58dce057c3039d41b5f5be38f
Coen van den Munckhof <c.munckhof@gmail.com>
2026-03-10 19:18:03 +0100

    add tests

--- tests/RepoM.Core.Repositories.Tests/Model/RepositoryInfoTests.cs
+++ tests/RepoM.Core.Repositories.Tests/Model/RepositoryInfoTests.cs
@@ -1,5 +1,6 @@
 namespace RepoM.Core.Repositories.Tests.Model;
 
+using System;
 using AwesomeAssertions;
 using RepoM.Core.Repositories.Model;
 using Xunit;
@@ -172,5 +173,257 @@ public class RepositoryInfoTests
     {
         var sut = CreateDefault();
         sut.Remotes.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnTrue_WhenSameReference()
+    {
+        var sut = CreateDefault();
+        sut.Equals(sut).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenOtherIsNull()
+    {
+        var sut = CreateDefault();
+        sut.Equals(null).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnTrue_WhenAllObservablePropertiesAreEqual()
+    {
+        var a = CreateDefault();
+        var b = CreateDefault();
+        a.Equals(b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_ShouldIgnoreLastSeen()
+    {
+        var a = CreateDefault();
+        a.LastSeen = DateTimeOffset.UtcNow;
+
+        var b = CreateDefault();
+        b.LastSeen = DateTimeOffset.UtcNow.AddHours(-1);
+
+        a.Equals(b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_ShouldIgnoreLastUpdated()
+    {
+        var a = CreateDefault();
+        a.LastUpdated = DateTimeOffset.UtcNow;
+
+        var b = CreateDefault();
+        b.LastUpdated = DateTimeOffset.UtcNow.AddDays(-1);
+
+        a.Equals(b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenSafePathDiffers()
+    {
+        var a = CreateDefault();
+        var b = new RepositoryInfo { Path = @"c:\repos\other", SafePath = "/repos/other", Name = "test", };
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenCurrentBranchDiffers()
+    {
+        var a = CreateDefault();
+        a.CurrentBranch = "main";
+
+        var b = CreateDefault();
+        b.CurrentBranch = "develop";
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenCurrentBranchHasUpstreamDiffers()
+    {
+        var a = CreateDefault();
+        a.CurrentBranchHasUpstream = true;
+
+        var b = CreateDefault();
+        b.CurrentBranchHasUpstream = false;
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenCurrentBranchIsDetachedDiffers()
+    {
+        var a = CreateDefault();
+        a.CurrentBranchIsDetached = true;
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenCurrentBranchIsOnTagDiffers()
+    {
+        var a = CreateDefault();
+        a.CurrentBranchIsOnTag = true;
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(nameof(RepositoryInfo.AheadBy))]
+    [InlineData(nameof(RepositoryInfo.BehindBy))]
+    [InlineData(nameof(RepositoryInfo.LocalUntracked))]
+    [InlineData(nameof(RepositoryInfo.LocalModified))]
+    [InlineData(nameof(RepositoryInfo.LocalMissing))]
+    [InlineData(nameof(RepositoryInfo.LocalAdded))]
+    [InlineData(nameof(RepositoryInfo.LocalStaged))]
+    [InlineData(nameof(RepositoryInfo.LocalRemoved))]
+    [InlineData(nameof(RepositoryInfo.LocalIgnored))]
+    [InlineData(nameof(RepositoryInfo.StashCount))]
+    public void Equals_ShouldReturnFalse_WhenNullableIntPropertyDiffers(string propertyName)
+    {
+        var a = CreateDefault();
+        typeof(RepositoryInfo).GetProperty(propertyName)!.SetValue(a, 5);
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenWasFoundDiffers()
+    {
+        var a = CreateDefault();
+        a.WasFound = false;
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenTagsDiffer()
+    {
+        var a = CreateDefault();
+        a.Tags = ["v1.0",];
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenBranchesDiffer()
+    {
+        var a = CreateDefault();
+        a.Branches = ["main", "develop",];
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_WhenLocalBranchesDiffer()
+    {
+        var a = CreateDefault();
+        a.LocalBranches = ["feature/x",];
+
+        var b = CreateDefault();
+
+        a.Equals(b).Should().BeFalse();
+    }
+
+    [Fact]
+    public void EqualsObject_ShouldReturnTrue_WhenEqual()
+    {
+        var a = CreateDefault();
+        object b = CreateDefault();
+
+        a.Equals(b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void EqualsObject_ShouldReturnFalse_WhenDifferentType()
+    {
+        var a = CreateDefault();
+        a.Equals("not a repo").Should().BeFalse();
+    }
+
+    [Fact]
+    public void EqualsObject_ShouldReturnFalse_WhenObjectIsNull()
+    {
+        var a = CreateDefault();
+        a.Equals((object?)null).Should().BeFalse();
+    }
+
+    // --- GetHashCode tests ---
+
+    [Fact]
+    public void GetHashCode_ShouldBeEqual_WhenObjectsAreEqual()
+    {
+        var a = CreateDefault();
+        a.CurrentBranch = "main";
+        a.AheadBy = 1;
+        a.BehindBy = 2;
+        a.StashCount = 3;
+
+        var b = CreateDefault();
+        b.CurrentBranch = "main";
+        b.AheadBy = 1;
+        b.BehindBy = 2;
+        b.StashCount = 3;
+
+        a.GetHashCode().Should().Be(b.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldDiffer_WhenSafePathDiffers()
+    {
+        var a = CreateDefault();
+        var b = new RepositoryInfo { Path = @"c:\repos\other", SafePath = "/repos/other", Name = "other", };
+
+        a.GetHashCode().Should().NotBe(b.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldDiffer_WhenCurrentBranchDiffers()
+    {
+        var a = CreateDefault();
+        a.CurrentBranch = "main";
+
+        var b = CreateDefault();
+        b.CurrentBranch = "develop";
+
+        a.GetHashCode().Should().NotBe(b.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldBeConsistent()
+    {
+        var sut = CreateDefault();
+        sut.CurrentBranch = "main";
+
+        var hash1 = sut.GetHashCode();
+        var hash2 = sut.GetHashCode();
+
+        hash1.Should().Be(hash2);
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldNotThrow_WhenAllNullableFieldsAreNull()
+    {
+        var sut = CreateDefault();
+
+        Action act = () => sut.GetHashCode();
+
+        act.Should().NotThrow();
     }
 }
